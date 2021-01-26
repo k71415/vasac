9089d5b82c4c6adac2f54f057bcbb36d52299146
박연주 <k71416@naver.com>
2021-01-27 02:31:05 +0900

    .

---
+++ vasac/src/main/java/com/vasac/domain/MemberVO.java
@@ -0,0 +1,71 @@
+package com.vasac.domain;
+
+import java.util.Date;
+
+public class MemberVO {
+	private String userId;
+	private String userPass;
+	private String userName;
+	private String userPhone;
+	private String userAddr1;
+	private String userAddr2;
+	private String userAddr3;
+	private Date regiDate;
+	private int verify;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserAddr1() {
+		return userAddr1;
+	}
+	public void setUserAddr1(String userAddr1) {
+		this.userAddr1 = userAddr1;
+	}
+	public String getUserAddr2() {
+		return userAddr2;
+	}
+	public void setUserAddr2(String userAddr2) {
+		this.userAddr2 = userAddr2;
+	}
+	public String getUserAddr3() {
+		return userAddr3;
+	}
+	public void setUserAddr3(String userAddr3) {
+		this.userAddr3 = userAddr3;
+	}
+	public Date getRegiDate() {
+		return regiDate;
+	}
+	public void setRegiDate(Date regiDate) {
+		this.regiDate = regiDate;
+	}
+	public int getVerify() {
+		return verify;
+	}
+	public void setVerify(int verify) {
+		this.verify = verify;
+	}
+	
+	
+}
